7afc34e49a8b89e33b67c0f2c67adf77992f1f84
syed abdul muqeeth <samx123786@gmail.com>
2026-06-08 23:49:05 +0530

    Update layout and site manifest for enhanced branding and mobile integration
    - Added favicon configurations in layout for various sizes and formats.
    - Updated site manifest with detailed application information, including name, description, and theme colors for better mobile experience.

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,22 @@ export const metadata: Metadata = {
   },
   description: siteConfig.description,
   applicationName: siteConfig.name,
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.ico", sizes: "any" },
+    ],
+    apple: [
+      {
+        url: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    shortcut: ["/favicon.ico"],
+  },
   keywords: [
     "Cosmocure",
     "cosmetic salon",
